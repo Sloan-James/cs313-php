b97e004675eef55ca4f3be54c00b0c5c5a071ad0
Sloan-James <staresatcomps@gmail.com>
2018-05-13 14:41:54 -0600

    more stuff

--- web/nav.php
+++ web/nav.php
@@ -6,7 +6,7 @@
 
     foreach($pages as $page){
        if(!in_array($page, $notWanted)){
-           $link = 'web/' . $page;
+           $link = $page;
            $safeName = strtoupper(str_replace('.html', '', $page));
            echo '<a href="' . $link . '">' . $safeName . '</a> </br>';
 
