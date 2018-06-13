16c8eb9ddf2bc8e4defc507aa32938717293f4e0
Sloan-James <staresatcomps@gmail.com>
2018-06-13 10:54:06 -0600

    Prove03

--- web/Assignments/Prove03/Cart.php
+++ web/Assignments/Prove03/Cart.php
@@ -10,22 +10,17 @@ session_start();
     <body>
         <p>Cart</p>
         <?php
-        for ($i = 1; $i <= 3; $i++){
-            echo "here2";
             foreach ( $_SESSION["cart"] as $key){
-                echo "here2";
                 if($key["qty"] > 0){
-                    echo "here3";
                 ?>
         <p><?php echo $key["item"];?>    
             $<?php echo $key["price"];?>     
                 <?php echo $key["qty"];?>   
-            $<?php echo ($key["price"] * $key["qty"]);?></p></br>
+            $<?php echo ($key["price"] * $key["qty"]);?></p>
             
         <?php
                 }
             }
-        }
          //print_r($_SESSION);
         ?>
     </body>
