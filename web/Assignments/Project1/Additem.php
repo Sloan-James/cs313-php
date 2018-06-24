2a22f801ca8a136263458ef6f8acf8091ffdfdbe
Sloan-James <staresatcomps@gmail.com>
2018-06-24 10:05:50 -0600

    Project 1

--- web/Assignments/Project1/Additem.php
+++ web/Assignments/Project1/Additem.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <h1>Add Items</h1>
         <strong>Please check that the expansion dropdown shows the correct expansion for the item you're adding.</strong>
-        <form id="" method="get" action="ParseAlla.php">
+        <form id="" method="post" action="ParseAlla.php">
             <input name="itemLink" style="width: 500px;" autocomplete="off" type="text">
             <br>
             <br>
