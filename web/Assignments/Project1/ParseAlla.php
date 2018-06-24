eb95d4c02dcea60cd90c4631bda7c831eb896d6f
Sloan-James <staresatcomps@gmail.com>
2018-06-24 11:00:45 -0600

    Project 1

--- web/Assignments/Project1/ParseAlla.php
+++ web/Assignments/Project1/ParseAlla.php
@@ -7,12 +7,14 @@ $dom = new DOMDocument();
 $dom->loadHTML($html);
 $xpath = new DomXPath($dom);
 echo "finder<br>";
-$items = $xpath->query('//div[contains(@class, "nobgrd")]');
+$items = $xpath->query('//div[@class="nobgrd"]');
+echo "query<br>";
+$item = $items->item(0);
+echo "item 1<br>";
+$test = $item->innerHTML;
+echo "innerhtml<br>";
 
-foreach ($items as $item){
-    echo "<br/>[". $item->nodeName. "]";
-    
-}
+echo $test;
 
 foreach($itemLines as $line){
     echo "Test 2<br>";
