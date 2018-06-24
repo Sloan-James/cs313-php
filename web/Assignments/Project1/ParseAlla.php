d4324644aff4bac012a520dac62eb3f141d033d7
Sloan-James <staresatcomps@gmail.com>
2018-06-24 16:15:18 -0600

    Project 1

--- web/Assignments/Project1/ParseAlla.php
+++ web/Assignments/Project1/ParseAlla.php
@@ -2,8 +2,15 @@
 
 echo "begin<br>";
 
-$html = $_POST["itemLink"];
+$url = $_POST["itemLink"];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+$html = curl_exec($ch);
+
 $dom = new DOMDocument();
+
 $dom->loadHTML($html);
 $xpath = new DomXPath($dom);
 echo "finder<br>";
