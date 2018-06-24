5c504e6b607e54c424d4b4b582888838011dbf85
Sloan-James <staresatcomps@gmail.com>
2018-06-24 10:17:22 -0600

    Project 1

--- web/Assignments/Project1/ParseAlla.php
+++ web/Assignments/Project1/ParseAlla.php
@@ -1,26 +1,26 @@
 <?php
 
-echo "begin";
+echo "begin<br>";
 
 $html = $_POST["itemLink"];
-echo "retrieve post";
+echo "retrieve post<br>";
 $dom = new DOMDocument();
-echo "new Document";
+echo "new Document<br>";
 $dom->loadHTML($html);
 
-echo "loaded html";
+echo "loaded html<br>";
 
-$item = $dom->getElementsByClassName("nobgrd");
+$item = $dom->getElementsByTagName("nobgrd");
 
-echo "Got class nobgrd";
+echo "Got class nobgrd<br>";
 
 $itemLines = split('<br>',$item);
 
-echo "Test";
+echo "Test<br>";
 echo $itemLines;
 
 foreach($itemLines as $line){
-    echo "Test 2";
+    echo "Test 2<br>";
     echo $line;
     echo "<br>";
 }
