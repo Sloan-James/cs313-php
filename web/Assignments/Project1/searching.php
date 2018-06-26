36d7c3a37ee87957915398b3d58dc667ae7b1e51
Sloan-James <staresatcomps@gmail.com>
2018-06-25 21:29:31 -0600

    Project 01

--- web/Assignments/Project1/searching.php
+++ web/Assignments/Project1/searching.php
@@ -7,6 +7,9 @@ $class = $_POST["class"];
 $race = $_POST["race"];
 $expansion = $_POST["expansion"];
 echo "received variables<br>";
+echo $class . "<br>";
+echo $race . "<br>";
+echo $expansion . "<br>";
 
 $stmt = $db->prepare("SELECT * FROM itemdb WHERE classes[:class] = '1' AND races[:race] = '1' AND expansion = :expansion");
 echo "prepared statement<br>";
